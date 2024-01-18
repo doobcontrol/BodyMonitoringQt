69a2aae3fb21b97570a5fd34e05d41c4fb85f038
doobcontrol <dooacontrol@protonmail.com>
2024-01-19 05:09:10 +0800

    整理dpkg package make script相关文件
    1，把所有相关文件移动到子目录 installfiles
    2，把control和postinst作为独立文件实现，而不是在dpkgdebmaker.sh中生成
    3，通过XDG_DESKTOP_DIR参数确定目标系统桌面目录
    
    尚存的问题：
    1，在ubuntu下安装后桌面程序不会自动allow launching，需要手动做一次操作
    2，在openKylin下能在shell下通过dpkg -i成功安装和卸载，但用其安装工具，直接双击运行安装会出错，不能成功安装

---
+++ installfiles/dpkgdebmaker.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Author : dooacontrol
+
+echo "dpkg deb make start:"
+
+mkdir -p BodyMonitoringQt-dpkg/usr/local/bin
+cp ../BodyMonitoringQt BodyMonitoringQt-dpkg/usr/local/bin
+cp ../BodyMonitoringQt.png BodyMonitoringQt-dpkg/usr/local/bin
+
+echo "work fold created and application files copyed!"
+
+mkdir -p BodyMonitoringQt-dpkg/usr/share/applications
+cp ./BodyMonitoringQt.desktop BodyMonitoringQt-dpkg/usr/share/applications
+
+echo "desktop file copyed!"
+
+mkdir -p BodyMonitoringQt-dpkg/DEBIAN
+
+cp ./control BodyMonitoringQt-dpkg/DEBIAN/control
+
+echo "control file copyed!"
+
+cp ./postinst BodyMonitoringQt-dpkg/DEBIAN/postinst
+chmod 0755 BodyMonitoringQt-dpkg/DEBIAN/postinst
+
+echo "postinst file copyed!"
+
+dpkg-deb -b BodyMonitoringQt-dpkg
+
+rm -r BodyMonitoringQt-dpkg
+echo "dpkg file created!"
+
+
