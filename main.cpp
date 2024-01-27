23faaf44e7e26af89fca9c974af3dc5259c9c1a0
doobcontrol <dooacontrol@protonmail.com>
2024-01-28 05:47:46 +0800

    对话框汉化
    需要加载qtbase_zh_CN.qm翻译文件。
    位置：/usr/share/qt5/translations/qtbase_zh_CN.qm
    手工拷贝到本项目资源目录，但并不加入GIT库

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 #include "bmMainWin.h"
 #include "dbHelper.h"
 #include "Equ.h"
+#include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
+    
+    //加载Qt标准对话框的中文翻译文件 (摘自网络)
+    //可从安装目录拷贝：/usr/share/qt5/translations/qtbase_zh_CN.qm
+    QTranslator tran;
+    bool ok = tran.load(":/qtbase_zh_CN.qm");
+    if(ok){
+    	app.installTranslator(&tran);
+    }
+    else{    
+        qDebug() << QString("加载汉化文件失败");
+    }
+    
     dbHelper::initXyBaseModelList.append(Equ::get());
     dbHelper::init();
     
